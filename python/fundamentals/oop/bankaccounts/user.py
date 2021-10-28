3e9535eafbd28950e7bbb4af4893bd238bd3b5d7
WinterP <winterp.dev@gmail.com>
2021-10-28 17:48:23 -0500

    Upload Update

--- python/fundamentals/oop/bankaccounts/user.py
+++ python/fundamentals/oop/bankaccounts/user.py
@@ -2,29 +2,30 @@ from bankaccount import bankAccount
 
 class User: 
     def __init__(self, user_name):
-        self.user_balance = bankAccount(int_rate=0.02, balance=0)
         self.user_name = user_name
+        self.account = bankAccount(int_rate=0.02, balance=0)
 
     def make_deposit(self,amount):
-        self.user_balance += amount
+        self.account.balance += amount
         return self
 
     def make_withdrawal(self,amount):
-        self.user_balance -= amount
+        self.account.balance -= amount
         return self
 
     def transfer_money(self,receiver,amount):
-        self.make_withdrawal(amount)
-        receiver.make_deposit(amount)
-        print(f"User {self.user_name} Transfered {amount} to {receiver.user_name}.\n User: {self.user_name} Balance: {self.user_balance} \n User: {receiver.user_name} Balance: {receiver.user_balance}")
+        self.account.withdraw(amount)
+        receiver.account.deposit(amount)
+        print(f"User {self.user_name} Transfered {amount} to {receiver.user_name}.\n User: {self.user_name} Balance: {self.account.balance} \n User: {receiver.user_name} Balance: {receiver.account.balance}")
         return self, receiver
 
     def display_user_balance(self):
-        print("User:" + self.user_name + " Balance:" + str(self.user_balance))
+        print("User:" + self.user_name + " Balance:" + str(self.account.balance))
         return self
 
 
 winter = User("Winter")
+winter_2 = User("Winter2")
 summer = User("Summer")
 autumn = User("Autumn")
 spring = User("Spring")
@@ -36,3 +37,5 @@ summer.make_deposit(100).make_deposit(100).make_withdrawal(100).make_withdrawal(
 autumn.make_deposit(300).make_withdrawal(100).make_withdrawal(100).make_withdrawal(100).display_user_balance()
 
 winter.transfer_money(autumn,50)
+
+winter.transfer_money(winter_2, 20)
